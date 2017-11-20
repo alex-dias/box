925cd939bc0da98026b3cdfac47b878cdb161b98
alex-dias <alex.d.negretti@gmail.com>
2017-11-20 09:09:50 -0200

    Adicionado reconhecimento de posição de imã

--- projeto.ino
+++ projeto.ino
@@ -3,8 +3,8 @@
 //Create a new software  serial
 SoftwareSerial bluetooth(0, 1); //TX, RX (Bluetooth)
   
-const int ledPin1 = 4;
-const int ledPin2 = 13;
+const int ledPin1 = 7;
+const int ledPin2 = 6;
 
 const int sensor1 = A1;
 const int sensor2 = A2;
@@ -15,52 +15,110 @@ int incomingByte;
 int sensorValue;
 int sensorValue2;
 
+int vetorDePecas[10] = {};
+int contador = 1;
+
 void setup() {
   //Initialize the software serial
   bluetooth.begin(9600);
   
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+
+
 }
 
 void loop() {
   sensorValue = analogRead(sensor1);
   sensorValue2 = analogRead(sensor2);
   
-  if(analogRead(sensor1) < 150){
+  if((sensorValue < 150) && (sensorValue2 < 150) && (vetorDePecas[(contador - 1)] == 0)){
+    vetorDePecas[contador] = 1;
+
     digitalWrite(ledPin1, HIGH);
-    
-    bluetooth.print("Led Vermelho (");
-    bluetooth.print(sensorValue);
-    bluetooth.println(")");
-    delay(200);
-  }
-  if(analogRead(sensor1) > 150){
-    digitalWrite(ledPin1, LOW);
-    
-    bluetooth.print("Led Vermelho (");
-    bluetooth.print(sensorValue);
-    bluetooth.println(")");
-    delay(200);
-  }
-
-  if(analogRead(sensor2) < 150){
     digitalWrite(ledPin2, HIGH);
-
-    bluetooth.print("Led Verde (");
-    bluetooth.print(sensorValue2);
-    bluetooth.println(")");
+    
+    bluetooth.println("Frente");
+    bluetooth.println(vetorDePecas[contador]);
+    bluetooth.println(contador);
     delay(200);
+
+    contador ++;
   }
-  
-  if(analogRead(sensor2) > 150){
+
+  if((sensorValue < 150) && (sensorValue2 > 150) && (vetorDePecas[(contador - 1)] == 0)){
+    vetorDePecas[contador] = 2;
+
+    digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, LOW);
     
-    bluetooth.print("Led Verde (");
-    bluetooth.print(sensorValue2);
-    bluetooth.println(")");
+    bluetooth.println("Esquerda");
+    bluetooth.println(vetorDePecas[contador]);
+    bluetooth.println(contador);
     delay(200);
+    
+    contador ++;
   }
+
+  if((sensorValue > 150) && (sensorValue2 < 150) && (vetorDePecas[(contador - 1)] == 0)){
+    vetorDePecas[contador] = 3;
+
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, HIGH);
+    
+    bluetooth.println("Direita");
+    bluetooth.println(vetorDePecas[contador]);
+    bluetooth.println(contador);
+    delay(200);
+    
+    contador ++;
+  }
+
+  if((sensorValue > 150) && (sensorValue2 > 150) && (vetorDePecas[(contador - 1)] != 0)){
+    vetorDePecas[contador] = 0;
+
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    
+    bluetooth.println("Vazio");
+    bluetooth.println(vetorDePecas[contador]);
+    bluetooth.println(contador);
+    delay(200);
+
+    contador ++;
+  }
+
+  /*
+
+  if(contador == 30){
+    for(int i = 0; i<30; i++){
+      if(vetorDePecas[i] == 1){
+        digitalWrite(ledPin1, HIGH);
+        digitalWrite(ledPin2, HIGH);
+        delay(1000);
+        digitalWrite(ledPin1, LOW);
+        digitalWrite(ledPin2, LOW);
+        delay(100);
+      }
+      if(vetorDePecas[i] == 2){
+        digitalWrite(ledPin1, HIGH);
+        delay(1000);
+        digitalWrite(ledPin1, LOW);
+        delay(100);
+      }
+      if(vetorDePecas[i] == 3){
+        digitalWrite(ledPin2, HIGH);
+        delay(1000);
+        digitalWrite(ledPin2, LOW);
+        delay(100);
+      }
+    }
+
+    contador = 1;
+  }
+
+  */
+
 }
 
 void piscaLed(int nomePorta, int tempo){
